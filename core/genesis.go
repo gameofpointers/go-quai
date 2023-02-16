84348170c334d3e58c58d84ebbab47e6ea224b4a
gop <gameofpointers@proton.me>
2023-02-16 17:32:15 -0600

    New garden knot

--- core/genesis.go
+++ core/genesis.go
@@ -387,7 +387,7 @@ func DefaultGardenGenesisBlock() *Genesis {
 	return &Genesis{
 		Config:     params.GardenChainConfig,
 		Knot:       ReadKnot("./core/knot/garden_knot.rlp"),
-		Nonce:      67,
+		Nonce:      69,
 		ExtraData:  hexutil.MustDecode("0x3535353535353535353535353535353535353535353535353535353535353535"),
 		GasLimit:   []uint64{1000000, 1000000, 1000000},
 		Difficulty: []*big.Int{big.NewInt(32048576), big.NewInt(8048576), big.NewInt(2048576)},
