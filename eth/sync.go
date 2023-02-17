0c38839ba07155c7bd09f26625c76b8fbc91df87
wizeguyy <wizeguyy@dominantstrategies.io>
2023-02-17 16:58:09 -0600

    bugfix: Increase channel size for overflowing channels

--- eth/sync.go
+++ eth/sync.go
@@ -171,7 +171,7 @@ type chainSyncOp struct {
 func newChainSyncer(handler *handler) *chainSyncer {
 	return &chainSyncer{
 		handler:     handler,
-		peerEventCh: make(chan struct{}),
+		peerEventCh: make(chan struct{}, 10),
 	}
 }
 
