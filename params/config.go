9058904d9c7029959875da401a039f1707635ae4
gop <gameofpointers@proton.me>
2023-11-29 18:58:40 -0600

    GENESIS

--- params/config.go
+++ params/config.go
@@ -34,7 +34,7 @@ var (
 
 	// Blake3GenesisHashes
 	Blake3PowColosseumGenesisHash  = common.HexToHash("0x40057f44be0809f939c7d70893d101abc74af1f1c535e406ec9909cfa2c20cbe")
-	Blake3PowGardenGenesisHash     = common.HexToHash("0xd382b6434b21c7828efba81742c47fb76c722c2d7d6aa00a925912f44b6524fc")
+	Blake3PowGardenGenesisHash     = common.HexToHash("0x31e72ffe9dbc0f5b7f7d9eff0a7b99219775f95addad97bec176faff414d02c9")
 	Blake3PowOrchardGenesisHash    = common.HexToHash("0x1f3743c323ec7a9a6a8150a33de81723abb70fa10ad49c35e365e48488430b56")
 	Blake3PowLocalGenesisHash      = common.HexToHash("0x31ac78ff6abcd99ad1fd8cd1b8ef54cfb28bdc72f7a38311e67a9e34fc2adb45")
 	Blake3PowLighthouseGenesisHash = common.HexToHash("0xaadedd7ed5a68a8885b54079e8193ae5be8f9b9bf4799ea9f1612bca835f33da")
