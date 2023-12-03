1729f11fa36c329fc2191e9f5b898b628f987996
gop <gameofpointers@proton.me>
2023-12-03 01:12:17 -0600

    Updated tghe diff to 4k

--- params/config.go
+++ params/config.go
@@ -34,7 +34,7 @@ var (
 
 	// Blake3GenesisHashes
 	Blake3PowColosseumGenesisHash  = common.HexToHash("0x40057f44be0809f939c7d70893d101abc74af1f1c535e406ec9909cfa2c20cbe")
-	Blake3PowGardenGenesisHash     = common.HexToHash("0xf710b7aed78476f09ce6ee1899cece9aca2445c0ef296fc718734e3a5c0271e5")
+	Blake3PowGardenGenesisHash     = common.HexToHash("0x6d3faa7b76a4c5a386b27ceda1dac56c1af3c1af8d14949355101fc313b40dad")
 	Blake3PowOrchardGenesisHash    = common.HexToHash("0x1f3743c323ec7a9a6a8150a33de81723abb70fa10ad49c35e365e48488430b56")
 	Blake3PowLocalGenesisHash      = common.HexToHash("0x31ac78ff6abcd99ad1fd8cd1b8ef54cfb28bdc72f7a38311e67a9e34fc2adb45")
 	Blake3PowLighthouseGenesisHash = common.HexToHash("0xaadedd7ed5a68a8885b54079e8193ae5be8f9b9bf4799ea9f1612bca835f33da")
