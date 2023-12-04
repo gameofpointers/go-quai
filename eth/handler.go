444c63b47fa74f0d681a7076c783a8893c8be623
gop <gameofpointers@proton.me>
2023-12-03 18:56:46 -0600

    Broadcast Block set to 150ms

--- eth/handler.go
+++ eth/handler.go
@@ -382,7 +382,7 @@ func (h *handler) BroadcastBlock(block *types.Block, propagate bool) {
 	peers := h.peers.peersWithoutBlock(hash)
 
 	// Sleep for 300 ms before broadcasting to all peers
-	time.Sleep(500 * time.Millisecond)
+	time.Sleep(150 * time.Millisecond)
 
 	// If propagation is requested, send to a subset of the peer
 	if propagate {
