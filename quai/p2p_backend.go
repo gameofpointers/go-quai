263f1278d795fcd9f4f1179f8c423150ea48274e
gop <gameofpointers@proton.me>
2024-01-12 15:37:34 -0600

    Implemented the GetHeight and the LookUpBlock methods

--- quai/p2p_backend.go
+++ quai/p2p_backend.go
@@ -1,6 +1,7 @@
 package quai
 
 import (
+	"context"
 	"github.com/dominant-strategies/go-quai/common"
 	"github.com/dominant-strategies/go-quai/core/types"
 	"github.com/dominant-strategies/go-quai/internal/quaiapi"
@@ -10,39 +11,46 @@ import (
 
 // QuaiBackend implements the quai consensus protocol
 type QuaiBackend struct {
-	primeBackend   *quaiapi.Backend
-	regionBackends []*quaiapi.Backend
-	zoneBackends   [][]*quaiapi.Backend
+	primeBackend   quaiapi.Backend
+	regionBackends []quaiapi.Backend
+	zoneBackends   [][]quaiapi.Backend
 
 	runningSlices map[types.SliceID]*types.Slice
 }
 
 // Create a new instance of the QuaiBackend consensus service
 func NewQuaiBackend() (*QuaiBackend, error) {
-	zoneBackends := make([][]*quaiapi.Backend, 1)
+	zoneBackends := make([][]quaiapi.Backend, 1)
 	for i := 0; i < 1; i++ {
-		zoneBackends[i] = make([]*quaiapi.Backend, 1)
+		zoneBackends[i] = make([]quaiapi.Backend, 1)
 	}
-	return &QuaiBackend{regionBackends: make([]*quaiapi.Backend, 1), zoneBackends: zoneBackends}, nil
+	return &QuaiBackend{regionBackends: make([]quaiapi.Backend, 1), zoneBackends: zoneBackends}, nil
 }
 
 // Set the PrimeBackend into the QuaiBackend
 func (qbe *QuaiBackend) SetPrimeBackend(primeBackend quaiapi.Backend) {
-	qbe.primeBackend = &primeBackend
+	qbe.primeBackend = primeBackend
 }
 
 // Set the RegionBackend into the QuaiBackend
 func (qbe *QuaiBackend) SetRegionBackend(regionBackend quaiapi.Backend, location common.Location) {
-	qbe.regionBackends[location.Region()] = &regionBackend
+	qbe.regionBackends[location.Region()] = regionBackend
 }
 
 // Set the ZoneBackend into the QuaiBackend
 func (qbe *QuaiBackend) SetZoneBackend(zoneBackend quaiapi.Backend, location common.Location) {
-	qbe.zoneBackends[location.Region()][location.Zone()] = &zoneBackend
+	qbe.zoneBackends[location.Region()][location.Zone()] = zoneBackend
 }
 
-func (qbe *QuaiBackend) GetBackend(sliceId types.SliceID) *quaiapi.Backend {
-	// TODO: Return the backened based on the sliceID and return it
+func (qbe *QuaiBackend) GetBackend(sliceId types.SliceID) quaiapi.Backend {
+	switch sliceId.Context.Level {
+	case common.PRIME_CTX:
+		return qbe.primeBackend
+	case common.REGION_CTX:
+		return qbe.regionBackends[sliceId.Region]
+	case common.ZONE_CTX:
+		return qbe.zoneBackends[sliceId.Region][sliceId.Zone]
+	}
 	return nil
 }
 
@@ -54,6 +62,7 @@ func (qbe *QuaiBackend) Start() error {
 // Handle blocks received from the P2P client
 func (qbe *QuaiBackend) OnNewBlock(sourcePeer p2p.PeerID, block types.Block) bool {
 	log.Info("New Block Found", "Block", block)
+	// TODO: Add the entropy logic
 	panic("todo")
 }
 
@@ -64,8 +73,8 @@ func (qbe *QuaiBackend) OnNewTransaction(sourcePeer p2p.PeerID, tx types.Transac
 
 // Returns the current block height for the given sliceID
 func (qbe *QuaiBackend) GetHeight(slice types.SliceID) uint64 {
-	// Example/mock implementation
-	panic("todo")
+	backend := qbe.GetBackend(slice)
+	return backend.CurrentHeader().NumberU64(int(slice.Context.Level))
 }
 
 func (qbe *QuaiBackend) GetSlice(slice types.SliceID) *types.Slice {
@@ -84,5 +93,10 @@ func (qbe *QuaiBackend) SetRunningSlices(slices []types.Slice) {
 }
 
 func (qbe *QuaiBackend) LookupBlock(hash common.Hash, slice types.SliceID) *types.Block {
-	panic("todo")
+	backend := qbe.GetBackend(slice)
+	block, err := backend.BlockByHash(context.Background(), hash)
+	if err != nil {
+		return nil
+	}
+	return block
 }
