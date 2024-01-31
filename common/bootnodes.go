6f293e7ecdfffb2e1914f013c873d1ba4f56a3ce
gop <gameofpointers@proton.me>
2024-01-31 14:45:21 -0600

    Bootnodes for testing
    SQSQ

--- common/bootnodes.go
+++ common/bootnodes.go
@@ -2,8 +2,8 @@ package common
 
 var (
 	BootstrapPeers = []string{
-		"/ip4/34.31.180.127/tcp/4001/p2p/12D3KooWJ8d3AnGzscdVf2kzxzTbJgDtDXqrHS8vnedrFmZr8uvK",
-		"/ip4/34.68.124.139/tcp/4001/p2p/12D3KooWLxXHZx2QGjJra2oWjGYQ3Vdrkew5cNZcEgfaiKi7Dfrx",
-		"/ip4/34.41.131.24/tcp/4001/p2p/12D3KooWD8kycTRgZAovnciaoyNzcJxW16hCFNSfJYm4Lj6ouCBd",
+		"/ip4/35.227.43.191/tcp/4001/p2p/12D3KooWFtYckZKyMtqkdjiQD2wMiPTT411ZWKLQhsv6hCUHbeNN",
+		"/ip4/34.30.223.152/tcp/4001/p2p/12D3KooWMqCFqMxswdr44E7Q6e8YGi2ifVv7ujvtpkgLovvSiDgF",
+		"/ip4/34.70.137.228/tcp/4001/p2p/12D3KooWF7q3aXNa1xt74NVMdRTJjrEHL1w3f4gSsWAARFQkFbXi",
 	}
 )
