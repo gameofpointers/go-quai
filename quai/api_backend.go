2e855fd7b80d91c08528079c03e921abc013f3a0
Hussam <hussam@dominantstrategies.io>
2024-05-02 16:14:07 -0500

    Make Broadcast an async method to prevent stalling

--- quai/api_backend.go
+++ quai/api_backend.go
@@ -578,22 +578,20 @@ func (b *QuaiAPIBackend) SubscribeExpansionEvent(ch chan<- core.ExpansionEvent) 
 ////////// P2P ///////////////
 //////////////////////////////
 func (b *QuaiAPIBackend) BroadcastWorkObject(wo *types.WorkObject, location common.Location) error {
-	errChan := make(chan error, 2)
-
-	go func() {
-		err := b.quai.p2p.Broadcast(location, wo.ConvertToBlockView())
-		errChan <- err
-	}()
-
-	go func() {
-		err := b.quai.p2p.Broadcast(location, wo.ConvertToHeaderView())
-		errChan <- err
-	}()
+	blockErrChan := b.quai.p2p.Broadcast(location, wo.ConvertToBlockView())
+	headerErrChan := b.quai.p2p.Broadcast(location, wo.ConvertToHeaderView())
 
 	// Return the error if any of the broadcasts fail
 	for i := 0; i < 2; i++ {
-		if err := <-errChan; err != nil {
-			return err
+		select {
+		case err := <-blockErrChan:
+			if err != nil {
+				return err
+			}
+		case err := <-headerErrChan:
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
