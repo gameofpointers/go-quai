e61772e0f5ea6046c7b8de29b8cefed142aeb188
Jonathan Downing <jonathan@dominantstrategies.io>
2025-03-26 13:40:35 -0500

    Added PUSH0, TLOAD, TSTORE, and MCOPY opcodes to EVM

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -18,10 +18,12 @@ package vm
 
 import (
 	"hash"
+	"math/big"
 	"sync/atomic"
 
 	"github.com/dominant-strategies/go-quai/common"
 	"github.com/dominant-strategies/go-quai/common/math"
+	"github.com/dominant-strategies/go-quai/params"
 )
 
 // Config are the configuration options for the Interpreter
@@ -166,6 +168,9 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 		op = contract.GetOp(pc)
 		operation := in.cfg.JumpTable[op]
 		if operation == nil {
+			return nil, &ErrInvalidOpCode{opcode: op}
+		} else if in.evm.Context.BlockNumber.Cmp(new(big.Int).SetInt64(params.MaxCodeSizeForkHeight)) < 0 && NewOpCodes[op] {
+			// The new opcodes are not allowed before the fork block
 			return nil, &ErrInvalidOpCode{opcode: op}
 		}
 		// Validate stack
