afda0a0e90563d0ebfe424dd1cf2ccda817f7362
gop <gameofpointers@proton.me>
2026-01-17 13:33:34 -0600

    Added separate subscribe chain event method to hc

--- core/slice.go
+++ core/slice.go
@@ -705,7 +705,8 @@ func (sl *Slice) Append(header *types.WorkObject, domTerminus common.Hash, domOr
 	time7 := common.PrettyDuration(time.Since(start))
 
 	if order == sl.NodeCtx() {
-		sl.hc.bc.chainFeed.Send(ChainEvent{Block: block, Hash: block.Hash(), Order: order, Entropy: sl.hc.TotalLogEntropy(block)})
+		sl.hc.bc.chainFeedForHC.Send(ChainEvent{Block: block, Hash: block.Hash(), Order: order, Entropy: sl.hc.TotalLogEntropy(block)})
+		go sl.hc.bc.chainFeed.Send(ChainEvent{Block: block, Hash: block.Hash(), Order: order, Entropy: sl.hc.TotalLogEntropy(block)})
 	}
 
 	if sl.NodeCtx() == common.ZONE_CTX && sl.ProcessingState() {
@@ -1198,7 +1199,7 @@ func (sl *Slice) GetKQuaiAndUpdateBit(hash common.Hash) (*big.Int, uint8, error)
 				_, order, calcOrderErr := sl.hc.CalcOrder(block)
 				if calcOrderErr == nil {
 					if order == sl.NodeCtx() {
-						sl.hc.bc.chainFeed.Send(ChainEvent{Block: block, Hash: block.Hash(), Order: order, Entropy: sl.hc.TotalLogEntropy(block)})
+						sl.hc.bc.chainFeedForHC.Send(ChainEvent{Block: block, Hash: block.Hash(), Order: order, Entropy: sl.hc.TotalLogEntropy(block)})
 					}
 				}
 			}
@@ -1215,7 +1216,7 @@ func (sl *Slice) GetKQuaiAndUpdateBit(hash common.Hash) (*big.Int, uint8, error)
 			_, order, calcOrderErr := sl.hc.CalcOrder(block)
 			if calcOrderErr == nil {
 				if order == sl.NodeCtx() {
-					sl.hc.bc.chainFeed.Send(ChainEvent{Block: block, Hash: block.Hash(), Order: order, Entropy: sl.hc.TotalLogEntropy(block)})
+					sl.hc.bc.chainFeedForHC.Send(ChainEvent{Block: block, Hash: block.Hash(), Order: order, Entropy: sl.hc.TotalLogEntropy(block)})
 				}
 			}
 		}
